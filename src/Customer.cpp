ef577a0813b9d955c9e1e93c6fb7f614e17fa76f
venkatavellanki <143627172+venkatavellanki@users.noreply.github.com>
2025-07-01 12:52:18 +0530

    Update Customer.cpp

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -7,3 +7,20 @@ void Customer::displayInfo() {
     std::cout << "Customer Name: " << name << "\n";
     std::cout << "Table Number: " << tableNumber << "\n";
 }
+
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <string>
+
+class Customer {
+private:
+    std::string name;
+    int tableNumber;
+
+public:
+    Customer(std::string n, int t);
+    void displayInfo();
+};
+
+#endif
